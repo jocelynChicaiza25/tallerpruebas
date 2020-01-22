61843b46075fe21b55b46bbbf5303bdbb25fe62f
Jocelyn Chicaiza <joanchic@espol.edu.ec>
2020-01-21 22:34:01 -0500

    test bonus

--- tallerpruebas/test/tallerpruebas/EmployeeTest.java
+++ tallerpruebas/test/tallerpruebas/EmployeeTest.java
@@ -11,6 +11,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static tallerpruebas.EmployeeType.Manager;
+import static tallerpruebas.EmployeeType.Supervisor;
+import static tallerpruebas.EmployeeType.Worker;
 
 /**
  *
@@ -40,29 +43,137 @@ public class EmployeeTest {
     /**
      * Test of cs method, of class Employee.
      */
+    
+    //test para employee worker con USD
     @Test
-    public void testCs() {
+    public void testCsWorker() {
         System.out.println("cs");
-        Employee instance = null;
+        Employee instance = new Employee(400,"USD",10,Worker);
         float expResult = 0.0F;
         float result = instance.cs();
         assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+    }
+    //test para employee worker diferente de USD
+     @Test
+    public void testCsWorker2() {
+        System.out.println("cs");
+        Employee instance = new Employee(400,"E",10,Worker);
+        float expResult = 0.0F;
+        float result = instance.cs();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    
+      //test para employee Supervisor con USD
+    @Test
+    public void testCsSupervisor() {
+        System.out.println("cs");
+        Employee instance = new Employee(400,"USD",10,Supervisor);
+        float expResult = 0.0F;
+        float result = instance.cs();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    //test para employee Supervisor diferente de USD
+     @Test
+    public void testCsSupervisor2() {
+        System.out.println("cs");
+        Employee instance = new Employee(400,"E",10,Supervisor);
+        float expResult = 0.0F;
+        float result = instance.cs();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+      //test para employee Manager con USD
+    @Test
+    public void testCsManager() {
+        System.out.println("cs");
+        Employee instance = new Employee(400,"USD",10,Manager);
+        float expResult = 0.0F;
+        float result = instance.cs();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    //test para employee Manager diferente de USD
+     @Test
+    public void testCsManager2() {
+        System.out.println("cs");
+        Employee instance = new Employee(400,"E",10,Manager);
+        float expResult = 0.0F;
+        float result = instance.cs();
+        assertEquals(expResult, result, 0.0);
+        
     }
 
     /**
      * Test of CalculateYearBonus method, of class Employee.
      */
+    
+    
+    //test calculate bonus para employee Worker  USD
     @Test
-    public void testCalculateYearBonus() {
+    public void testCalculateYearBonusWorker() {
         System.out.println("CalculateYearBonus");
-        Employee instance = null;
-        float expResult = 0.0F;
+        Employee instance = new Employee(400,"USD",10,Worker);;
+        float expResult = 386.0F;
         float result = instance.CalculateYearBonus();
         assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+    }
+     
+    //test calculate bonus para employee Worker diferente de USD
+      @Test
+    public void testCalculateYearBonusWorker2() {
+        System.out.println("CalculateYearBonus");
+        Employee instance = new Employee(400,"E",10,Worker);
+        float expResult = 386.0F;
+        float result = instance.CalculateYearBonus();
+        assertEquals(expResult, result, 0.0);
     }
     
+    //test calculate bonus para employee Supervisor con  USD
+    @Test
+    public void testCalculateYearBonusSupervisor() {
+        System.out.println("CalculateYearBonus");
+        Employee instance = new Employee(400,"USD",10,Supervisor);
+        float expResult = 593.0F;
+        float result = instance.CalculateYearBonus();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+     
+    //test calculate bonus para employee Worker diferente de USD
+    @Test
+    public void testCalculateYearBonusSupervisor2() {
+        System.out.println("CalculateYearBonus");
+        Employee instance = new Employee(400,"E",10,Supervisor);;
+        float expResult = 573.0F;
+        float result = instance.CalculateYearBonus();
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    //test calculate bonus para employee Manager con  USD
+    @Test
+    public void testCalculateYearBonusManager() {
+        System.out.println("CalculateYearBonus");
+        Employee instance = new Employee(400,"USD",10,Manager);
+        float expResult = 786.0F;
+        float result = instance.CalculateYearBonus();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+     
+    //test calculate bonus para employee Manager diferente de USD
+    @Test
+    public void testCalculateYearBonusManager2() {
+        System.out.println("CalculateYearBonus");
+        Employee instance = new Employee(400,"E",10,Manager);
+        float expResult = 766.0F;
+        float result = instance.CalculateYearBonus();
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    
+  
 }
